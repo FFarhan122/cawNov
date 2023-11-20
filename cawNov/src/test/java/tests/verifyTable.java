53fc94df079ebad3eee0e7d8d8452d9f20325672
hp <hp@DESKTOP-ADC0TJP.mshome.net>
2023-11-21 05:08:45 +0530

    code

--- cawNov/src/test/java/tests/verifyTable.java
+++ cawNov/src/test/java/tests/verifyTable.java
@@ -1,56 +1,81 @@
 package tests;
 
-import java.time.Duration;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+import java.lang.Object;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
-import tests.Data;
-
-public class verifyTable extends Data
+public class verifyTable
 {
+	private static String readJsonFile(String filePath) 
+	{
+        // Read the entire JSON file as a string
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "{}"; // Return an empty JSON object if an error occurs
+        }
+    }
+	
 	static String text;
 	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\browserFile\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+		driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
+		
+		WebElement tableData = driver.findElement(By.xpath("//summary[text() = 'Table Data']"));
+		tableData.click();
+		
+		WebElement insertBox = driver.findElement(By.xpath("//textarea[@id='jsondata']"));
+		insertBox.clear();
+		String jsonString = readJsonFile("C:\\Users\\hp\\git\\cawNov\\cawNov\\src\\test\\java\\tests\\data.json");
+		insertBox.sendKeys(jsonString);
+		
+		WebElement refreshButton = driver.findElement(By.xpath("//button[@id='refreshtable']"));
+		refreshButton.click();
+		Object obj = JSONValue.parse(jsonString);
+	    JSONArray array = (JSONArray)obj;
+           
+		for (int i = 0; i < array.size(); i++)
 		{
-			System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\browserFile\\chromedriver.exe");
-			
-			WebDriver driver = new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-			driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
-			
-			WebElement tableData = driver.findElement(By.xpath("//summary[text() = 'Table Data']"));
-			tableData.click();
-			
-			WebElement insertBox = driver.findElement(By.xpath("//textarea[@id='jsondata']"));
-			insertBox.clear();
-			insertBox.sendKeys(Data.data());
-			
-			WebElement refreshButton = driver.findElement(By.xpath("//button[@id='refreshtable']"));
-			refreshButton.click();
-	
-			
-			System.out.println(s);
-			
-			for(int j = 2;j<7;j++)
-			{
-			List<WebElement> table = driver.findElements(By.xpath("//table[@id='dynamictable']//tr["+j+"]//td"));
-			int cellSize = table.size();
+			JSONObject jsonLineItem = (JSONObject) array.get(i);
+			String name = jsonLineItem.get("name").toString();
+			String age  = jsonLineItem.get("age").toString();
+			String gender = jsonLineItem.get("gender").toString();
+			System.out.print(name +" ");
+			System.out.print(age +" ");
+			System.out.print(gender +" "); 
+			System.out.println();
+	     }
+		System.out.println();
+		
+		for(int j= 2;j<8;j++)
+		{
+			List<WebElement> cells = driver.findElements(By.xpath("//table[@id='dynamictable']//tr["+j+"]"));
+			int cellSize = cells.size();
 			for(int i = 0;i<cellSize;i++)
 			{
-				text =table.get(i).getText()+ ",";
-				System.out.print(text);
+				text =cells.get(i).getText();
+				System.out.print(text +" ");
 			}
 			System.out.println();
-			}
-			
-			
-			Assert.assertEquals(text,exp);
 		}
-
+		
+		Assert.assertEquals(text,"Jennifer 42 female");
+	}
+		
 }
